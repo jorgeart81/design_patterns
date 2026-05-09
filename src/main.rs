42358b3756b58ab9a93ce488bb0a4967704f0481
Jorge Reyes <jorgeart81dev@gmail.com>
2026-05-08 23:06:07 -0500

    add adapter pattern

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::{
+    adapter::{OrdinaryTarget, SpecificTarget, Target, TargetAdapter},
     builder::{
         builders::{Builder, CarBuilder, CarManualBuilder},
         cars::{Car, Manual},
@@ -7,10 +8,12 @@ use crate::{
     factory_method::init::initialize,
 };
 
+mod adapter;
 mod builder;
 mod factory_method;
 
 fn main() {
+    run_adapter();
     run_builder();
     // run_factory_method();
 }
@@ -42,4 +45,33 @@ fn run_builder() {
     // The final car manual.
     let manual: Manual = manual_builder.build();
     println!("Car manual built:\n{}", manual);
+}
+
+/// Calls any object of a `Target` trait.
+///
+/// To understand the Adapter pattern better, imagine that this is
+/// a client code, which can operate over a specific interface only
+/// (`Target` trait only). It means that an incompatible interface cannot be
+/// passed here without an adapter.
+fn call(target: impl Target) {
+    println!("'{}'", target.request());
+}
+
+fn run_adapter() {
+    let target = OrdinaryTarget;
+
+    print!("A compatible target can be directly called: ");
+    call(target);
+
+    let adaptee = SpecificTarget;
+
+    println!(
+        "Adaptee is incompatible with client: '{}'",
+        adaptee.specific_request()
+    );
+
+    let adapter = TargetAdapter::new(adaptee);
+
+    print!("But with adapter client can call its method: ");
+    call(adapter);
 }
